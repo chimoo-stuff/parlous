5720fa4ab2b5a5127b858d02877df96b4f432db3
stackerv8 <92987125+stackerv8@users.noreply.github.com>
2022-01-01 19:39:55 +0800

    Movement Script Update(fix)
    Updated movement script and moved into chicken.gd

--- characters/scripts/chicken.gd
+++ characters/scripts/chicken.gd
@@ -5,14 +5,44 @@ var body_data = {}
 onready var age = 0
 var health = 1
 
+var MAX_SPEED = 500
+var ACCELERATION = 2000
+var motion = Vector2.ZERO
+
+func _physics_process(delta):
+	var axis = get_input_axis()
+	if axis == Vector2.ZERO:
+		apply_friction(ACCELERATION + delta)
+	else:
+		appy_movement(axis * ACCELERATION * delta)
+	motion = move_and_slide(motion)
+
+func get_input_axis():
+	var axis = Vector2.ZERO
+	axis.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
+	axis.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
+	return axis.normalized()
+	
+func apply_friction(ammount):
+	if motion.length() > ammount:
+		motion -= motion.normalized() * ammount
+	else:
+		motion = Vector2.ZERO
+
+func appy_movement(acceleration):
+	motion += acceleration
+	motion = motion.clamped(MAX_SPEED)
+
+
 var inventory = {}
 
 func _ready():
 	pass
 
-func _process(delta):
-	# Do not uncomment this.
-	animate_body()
+#func _process(delta):
+	 #Do not uncomment this.
+	#animate_body()
+#wasnt working and my brain is smol so i just commented it out
 
 """
 animate_body
